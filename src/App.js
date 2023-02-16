7e1c31f772e4afad486d88b4287031a8a722ec85
sudiptadip <sudiptabhattacharjee000@gmail.com>
2023-02-16 16:26:07 +0530

    add data

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AllRoute from './routes/AllRoute';
+import {Box} from "@chakra-ui/react"
+
+function App() {
+  return (
+    <Box className="App">
+      <AllRoute/>
+    </Box>
+  );
+}
+
+export default App;
